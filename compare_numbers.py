74740ecb35ac8e96bc2be1dbbd403c9b1b84fe8a
Pumelela-Banca <pumelelabanca@gmail.com>
2023-12-20 18:51:14 +0200

    compares user numberr to numbers entered into database

--- compare_numbers.py
+++ compare_numbers.py
@@ -27,11 +27,11 @@ def compare_numbers(user_numbers):
     # data is a list with [(id, date, numbers), (id, date, numbers)]
     data = result.fetchall()
     plays = len(data)
-    wins = 0                              # amount of money won
+    wins_user = 0                              # amount of money won
     for nums in data:
-        wins += moneyFunc.count_wins(user_numbers, str_to_list(nums[2]))
+        wins_user += moneyFunc.count_wins(user_numbers, str_to_list(nums[2]))
 
-    return wins, plays
+    return wins_user, plays
 
 
 def str_to_list(value: str):
@@ -41,3 +41,10 @@ def str_to_list(value: str):
     """
     new = [int(x) for x in value.split(", ")]
     return new
+
+
+if __name__ == "__main__":
+    # Test  for jack pot
+    print(compare_numbers([8, 9, 11, 32, 33]))
+    # Test for random
+    print(compare_numbers([8, 9, 11, 16, 12]))
